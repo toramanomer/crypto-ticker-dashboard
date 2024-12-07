5810a5b83f3f9c1bd2371b4b37e6dbe3a145afb2
Ömer Toraman <toramanomer@icloud.com>
2024-12-08 00:48:15 +0300

    update binance service to use different endpoint

--- backend/src/binance/binance.service.ts
+++ backend/src/binance/binance.service.ts
@@ -3,6 +3,15 @@ import { HttpService } from '@nestjs/axios'
 import { lastValueFrom, catchError } from 'rxjs'
 import { AxiosError } from 'axios'
 
+interface SymbolDetails {
+	symbol: string
+	priceChange: string
+	priceChangePercent: string
+	lastPrice: string
+	highPrice: string
+	lowPrice: string
+}
+
 @Injectable()
 export class BinanceService {
 	private readonly BASE_URL = 'https://api.binance.com/api/v3'
@@ -10,15 +19,26 @@ export class BinanceService {
 	constructor(private readonly httpService: HttpService) {}
 
 	async getTradingPairs() {
-		const url = `${this.BASE_URL}/ticker/price`
 		const response = await lastValueFrom(
-			this.httpService.get(url).pipe(
-				catchError((error: AxiosError) => {
-					throw new ServiceUnavailableException()
-				})
-			)
+			this.httpService
+				.get<SymbolDetails[]>(this.BASE_URL.concat('/ticker/24hr'))
+				.pipe(
+					catchError((error: AxiosError) => {
+						throw new ServiceUnavailableException()
+					})
+				)
 		)
 
-		return response.data.slice(0, 100)
+		return response.data
+			.sort((a, b) => parseFloat(b.lastPrice) - parseFloat(a.lastPrice))
+			.slice(0, 100)
+			.map(pair => ({
+				symbol: pair.symbol,
+				lastPrice: pair.lastPrice,
+				highPrice: pair.highPrice,
+				lowPrice: pair.lowPrice,
+				priceChange: pair.priceChange,
+				priceChangePercent: pair.priceChangePercent
+			}))
 	}
 }
